d29cdd2030417a8e32d0a9c27c15a6ed401bf4d9
daithyr <159961434+daithyr@users.noreply.github.com>
2024-04-01 23:20:18 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,40 +1,36 @@
-import altair as alt
-import numpy as np
-import pandas as pd
 import streamlit as st
+import pandas as pd
+import numpy as np
 
-"""
-# Welcome to Streamlit!
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+chart_data = pd.DataFrame(np.random.randn(20, 4), columns=["col1", "Angry", "Sad", "Happy"])
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+st.balloons()
+st.header("Hi, I am Daithy")
+col1, col2 = st.columns(2)
 
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
+with col1:
+    st.subheader("Education")
+    st.write("University of Washington - Technology Innovation")
+    st.subheader("Work Experience")
+    st.write("SDE internship @ DragonTesting")
+    st.subheader("Hobbies")
+    st.write("Sleep")
+    st.subheader("Interesting project")
+    st.write("none")
 
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
+with col2:
+    st.image('chelsey.JPG', caption='Me')
+    st.link_button("LinkedIn", "https://www.linkedin.com/in/chinshanlee/", help=None, type="secondary", disabled=False, use_container_width=False)
 
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
+st.divider()
 
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
+st.header("My Favorite Song")
+st.video("https://youtu.be/wA3mVqo-jvg?si=6fWBvft5O7-k2TcY", format="video/mp4", start_time=0)
 
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
+st.divider()
+
+st.header("My Mental Health Status")
+st.line_chart(
+   chart_data, x="col1", y=["Angry", "Sad", "Happy"], color=["#E07A5F", "#A9BCD0", "#F2CC8F"]
+)
